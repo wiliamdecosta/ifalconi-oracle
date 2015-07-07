db72bfdf7d94adc43d9330582e04e2a61d12e28e
chatsoe <chatsoe@yahoo.co.id>
2015-07-07 15:01:54 +0700

    ''

---
+++ bil_report/INVOICE_REPORT_events.php
@@ -0,0 +1,31 @@
+<?php
+//BindEvents Method @1-D40060DD
+function BindEvents()
+{
+    global $CCSEvents;
+    $CCSEvents["BeforeShow"] = "Page_BeforeShow";
+}
+//End BindEvents Method
+
+//Page_BeforeShow @1-BDAA1797
+function Page_BeforeShow(& $sender)
+{
+    $Page_BeforeShow = true;
+    $Component = & $sender;
+    $Container = & CCGetParentContainer($sender);
+    global $INVOICE_REPORT; //Compatibility
+//End Page_BeforeShow
+
+//Custom Code @129-2A29BDB7
+// -------------------------
+    // Write your own code here.
+// -------------------------
+//End Custom Code
+
+//Close Page_BeforeShow @1-4BC230CD
+    return $Page_BeforeShow;
+}
+//End Close Page_BeforeShow
+
+
+?>
